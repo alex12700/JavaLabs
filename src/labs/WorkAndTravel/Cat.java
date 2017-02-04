95458c70ed6d45741095757b3f96063a30fe7e44
alex12700 <arhipov_sasha127@mail.ru>
2017-02-04 12:28:22 +0500

    thats what i want

--- src/labs/WorkAndTravel/Cat.java
+++ src/labs/WorkAndTravel/Cat.java
@@ -1,9 +1,14 @@
-package labi;
+package labs.example;
 
-/**
- * Created by Stud on 27.01.2017.
- */
-public class Cat {
-    String name;
-    //nujno propisat' konstructor dli sozdaniya imeni i nazvaniya
+import static java.lang.Thread.sleep;
+
+public class Ex1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Отсчёт пошёл... ");
+        for(int i = 5; i >= 0; i--){
+            sleep(1000);
+            System.out.println(i);
+        }
+        System.out.println("BTOOOOOOM");
+    }
 }
