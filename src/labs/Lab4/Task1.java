48256067bd84f13b9f36652801788ae01ac3227a
alex12700 <arhipov_sasha127@mail.ru>
2017-02-05 23:13:51 +0500

    сделана 4 лаба, но была заморочка с методом и массивом в чем проблема так и не понятно но работает. была загвоздка с созданием диапазона, но решена. осталось только узнать как работает экспериментальная проверка для 4 задания. потом можно будет подчистить код

---
+++ src/labs/Lab4/Task1.java
@@ -0,0 +1,20 @@
+package labs.Lab4;
+
+/**
+ * 1.	Создать статический метод, который будет иметь два целочисленных параметра a и b,
+ * и в качестве своего значения возвращать случайное целое число из отрезка [a;b].
+ * C помощью данного метода заполнить массив из 20 целых чисел и вывести его на экран.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = two_variables(two_variables(100, 0), two_variables(100, 0));
+            System.out.println(array[i]);
+        }
+    }
+
+    public static int two_variables(int a, int b) {
+        return (int) (Math.random() * (b-a + 1) + a);
+    }
+}
---
+++ src/labs/Lab4/Task2.java
@@ -0,0 +1,36 @@
+package labs.Lab4;
+
+/**
+ * 2.	Создать метод, который будет выводить указанный массив на экран в строку.
+ * С помощью созданного метода и метода из предыдущей задачи заполнить 5 массивов
+ * из 10 элементов каждый случайными числами и вывести все 5 массивов на экран, каждый на отдельной строке
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        Task1 random = new Task1();
+        int[] array1 = new int[20];
+        int[] array2 = new int[20];
+        int[] array3 = new int[20];
+        int[] array4 = new int[20];
+        int[] array5 = new int[20];
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = random.two_variables(20,10);
+            array2[i] = random.two_variables(20,10);
+            array3[i] = random.two_variables(20,10);
+            array4[i] = random.two_variables(20,10);
+            array5[i] = random.two_variables(20,10);
+        }
+        output_array(array1);
+        output_array(array2);
+        output_array(array3);
+        output_array(array4);
+        output_array(array5);
+    }
+
+    public static void output_array(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+}
